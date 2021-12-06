5ec6717d27544167f7c82615e494a256d2f2f065
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2021-12-05 20:33:51 -0500

    Update students and teachers queries against SQL injections

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,12 +1,16 @@
-pool.query(`
+const quesryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
+const cohortName = process.argv[2] || 'JUL02';
+const values = [`%${cohortName}%`];
+
+pool.query(quesryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
